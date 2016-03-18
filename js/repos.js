289698d35c5ac80eed2c672b940b8f1cd41ed8ce
Jordan Meier <jbirdmeier@gmail.com>
2016-03-18 09:34:38 -0700

    feed the getRepos function a user name and change the api url to include the username

--- js/repos.js
+++ js/repos.js
@@ -1,7 +1,8 @@
 var apiKey = require('./../.env').apiKey;
 
-exports.getRepos = function(){
-  $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
+exports.getRepos = function(user) {
+  // var user = $('input#user').val();
+  $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
   }).fail(function(error){
     console.log(error.responseJSON.message);
