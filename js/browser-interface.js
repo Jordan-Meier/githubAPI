68b42d104286dccb62f1667a9ff2e9a0369e9426
Jordan Meier <jbirdmeier@gmail.com>
2016-03-18 11:57:30 -0700

    hide and show showRepos div depending on whether or not the form is submitted

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -2,6 +2,7 @@ var apiKey = require('./../.env').apiKey;
 var getRepos = require('./../js/repos.js').getRepos;
 
 $(document).ready(function () {
+  $('#showRepos').hide();
   $('#getRepos').click(function () {
     var user = $('input#userName').val();
     $("#userPhoto").empty();
@@ -9,7 +10,7 @@ $(document).ready(function () {
     $("#githubUserName").empty();
     $("#showFollowers").empty();
     $("#repos").empty();
-
+    $('#showRepos').show();
     getRepos(user);
 
   });
