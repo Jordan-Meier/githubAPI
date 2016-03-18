d9b3f583fabed8b3a10434cde19f698ca778378b
Jordan Meier <jbirdmeier@gmail.com>
2016-03-18 11:52:47 -0700

    add jQuery to clear out all fields when form is resubmitted

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -4,6 +4,11 @@ var getRepos = require('./../js/repos.js').getRepos;
 $(document).ready(function () {
   $('#getRepos').click(function () {
     var user = $('input#userName').val();
+    $("#userPhoto").empty();
+    $("#showUserName").empty();
+    $("#githubUserName").empty();
+    $("#showFollowers").empty();
+    $("#repos").empty();
 
     getRepos(user);
 
