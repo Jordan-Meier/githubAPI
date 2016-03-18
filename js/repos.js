793b2ad39eea2ebc6ca6d6272a7ff60df36e1932
Jordan Meier <jbirdmeier@gmail.com>
2016-03-18 11:26:46 -0700

    display the date last updated in a more legible form

--- js/repos.js
+++ js/repos.js
@@ -18,9 +18,7 @@ exports.getRepos = function(user) {
     // console.log(response);
 
     for (var i =0; i <= response.length; i++){
-      var last_updated = Date.UTC(response[i].updated_at);
-      console.log(last_updated);
-          last_updated = moment.unix(last_updated).format("MM/DD/YYYY");
+      var last_updated = new Date(response[i].updated_at);
       $('#repos').append("<div id='singleRepo'><h5> Repository Name: " + response[i].name + "</h5><h5> Date Last Updated: " + last_updated + "</h5><h5>Repository Description: " + response[i].description + ".</h5></div><br>");
     }
   }).fail(function(error){
