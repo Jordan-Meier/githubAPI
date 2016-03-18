808c56bec00568e81bef547ba060de831bd61742
Jordan Meier <jbirdmeier@gmail.com>
2016-03-18 10:34:08 -0700

    add a separate .get call to access only user info

--- js/repos.js
+++ js/repos.js
@@ -2,9 +2,19 @@ var apiKey = require('./../.env').apiKey;
 
 exports.getRepos = function(user) {
   // var user = $('input#user').val();
+  $.get('https://api.github.com/users/' + user + '?access_token=' + apiKey).then(function(response){
+    $('#userPhoto').append("<img src='" + response.avatar_url + "'>");
+    $('#showFollowers').text("Number of Followers: " + response.followers);
+    $('#githubUserName').text("Github User Name: " + response.login);
+    $('#showUserName').text(response.name + "'s Github Info");
+
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+
+
   $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
-    $('#showUserName').text("User Name: " + response[0].owner.login);
 
     for (var i =0; i <= response.length; i++){
       // var date = moment.unix(response.bikes[i].date_stolen).format("MM/DD/YYYY");
