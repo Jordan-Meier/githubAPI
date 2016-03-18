c4d95865458f1aab0eb60737fe47f82eff61793e
Jordan Meier <jbirdmeier@gmail.com>
2016-03-18 10:01:23 -0700

    add jQuery logic to display users repo names and descriptions

--- js/repos.js
+++ js/repos.js
@@ -5,6 +5,11 @@ exports.getRepos = function(user) {
   $.get('https://api.github.com/users/' + user + '/repos?access_token=' + apiKey).then(function(response){
     console.log(response);
     $('#showUserName').text("User Name: " + response[0].owner.login);
+
+    for (var i =0; i <= response.length; i++){
+      // var date = moment.unix(response.bikes[i].date_stolen).format("MM/DD/YYYY");
+      $('#repos').append("<li> Repository Name: " + response[i].name + ". Repository Description: " + response[i].description + ". </li>");
+    }
   }).fail(function(error){
     console.log(error.responseJSON.message);
   });
